adede679a8ea819b9ccb44b68af1b44d951516b3
EmelVv <vmemelianov0906@gmail.com>
2024-08-16 22:38:09 +0300

    save

--- vars.js
+++ vars.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 YANDEXKEY = "https://api-maps.yandex.ru/2.1/?apikey=ad6de3ca-b0ff-402f-9c98-678f49588a8e&lang=ru_RU"
 IP = "" // Можно указать домен. Оставить пустым для отображения глобального адреса сервера.
-PORT = 3000
+PORT = 80
 
 module.exports = {
     YANDEXKEY,
